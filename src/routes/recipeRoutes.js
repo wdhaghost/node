ea198b5583d92fcc0c1909eca90606aae0c71fc7
Coulibaly Abdourahmane <abdou.coulibaly@outlook.fr>
2024-05-02 14:03:56 +0200

    recipe router

---
+++ src/routes/recipeRoutes.js
@@ -0,0 +1,7 @@
+import { getRecipeByIngredient, getRecipeByName,index} from '../controllers/recipeController.js'
+ export default async function recipesRoutes (fastify,options){
+    fastify.get('/',index)
+    fastify.get('/recipes/:name',getRecipeByName)
+    fastify.get('/recipes/ingredient/:ingredient',getRecipeByIngredient)
+}
+
