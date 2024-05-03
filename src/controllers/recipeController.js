dfb5de32d84519e9376d642909da40ce82724b4d
Coulibaly Abdourahmane <abdou.coulibaly@outlook.fr>
2024-05-03 16:53:30 +0200

    recipe controller

--- src/controllers/recipeController.js
+++ src/controllers/recipeController.js
@@ -1,15 +1,42 @@
 import fastify from "fastify"
+import {Recipe} from "../models/Recipe.js"
+
 export  async function index (req,rep){
 
-
-   return {data:'recipes'}
+  const recipes = await Recipe.find()
+rep.send( recipes)
 }
 export  async function getRecipeByName (req,rep){
+  try {
+    const regex = new RegExp(req.params.name, 'i'); // 'i' for case insensitive
+    const recipes = await Recipe.find({ name: regex });
+    rep.send( recipes);
+  } catch (error) {
+    console.error('Error searching recipes:', error);
+  }
 
-   return {recette:req.params.name}
 }
 
 export  async function getRecipeByIngredient (req,rep){
-   return {ingredients:req.params.ingredient}
+  try {
+    const regex = new RegExp(req.params.ingredient, 'i'); // 'i' for case insensitive
+    const recipes = await Recipe.find({ ingredients: regex });
+    rep.send( recipes);
+  } catch (error) {
+    console.error('Error searching recipes:', error);
+  }
 
+}
+
+export async function createRecipe (recipe) {
+   try {
+      const { name, ingredients, steps } = recipe;
+      const newRecipe = new Recipe({name:name,ingredients:ingredients,steps:steps})
+      newRecipe.save();
+      console.log('Recipe created successfully:', newRecipe);
+
+    } catch (error) {
+      console.error('Error creating recipe:', error);
+      throw error;  // Rethrow the error if you want to handle it further up the call stack
+    }
 }
