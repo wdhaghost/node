be62ee7a292637a438ffd36a937fd7cb4f147077
Coulibaly Abdourahmane <abdou.coulibaly@outlook.fr>
2024-05-03 16:55:22 +0200

    server

--- server.js
+++ server.js
@@ -1,11 +1,39 @@
 import Fastify from 'fastify'
 import recipesRoutes from './src/routes/recipeRoutes.js'
+import dbConnector from './src/db/dbConnector.js'
+import fastifySwagger from "@fastify/swagger";
+import fastifySwaggerUi from "@fastify/swagger-ui";
+import fastifyCors from '@fastify/cors';
+
+
 const fastify = Fastify({
   logger: true
 })
 
-fastify.register(recipesRoutes)
+const swaggerOptions = {
+  swagger: {
+      info: {
+          title: "Hotwings API",
+          description: "dual DB API",
+          version: "1.0.0",
+      },
+      host: "127.0.0.1:3000",
+  },
+};
 
+const swaggerUiOptions = {
+  routePrefix: "/docs",
+  exposeRoute: true,
+};
+
+fastify.register(fastifySwagger, swaggerOptions);
+fastify.register(fastifySwaggerUi, swaggerUiOptions);
+fastify.register(dbConnector)
+
+fastify.register(recipesRoutes)
+fastify.register(fastifyCors,{
+  origin: true
+})
   fastify.listen({ port: 3000 }, function (err, address) {
     if (err) {
       fastify.log.error(err)
