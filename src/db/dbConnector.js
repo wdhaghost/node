a2ec7a206c0fd6ae989767b5ac957ff03d33f005
Coulibaly Abdourahmane <abdou.coulibaly@outlook.fr>
2024-05-31 17:15:49 +0200

    fix db connect

--- src/db/dbConnector.js
+++ src/db/dbConnector.js
@@ -1,8 +1,7 @@
 import fastifyPlugin from "fastify-plugin";
 import mongoose from "mongoose";
-
 async function dbConnector(fastify,options){
-await mongoose.connect(process.env.MONGO_URL_DB,{ serverSelectionTimeoutMS: 30000 })
+await mongoose.connect(process.env.MONGO_DB_URL,{ serverSelectionTimeoutMS: 30000 })
 .then(() => {
     console.log('MongoDB connected successfully');
   }).catch(err => {
