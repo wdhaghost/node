38a352da16b0b457024900ccefcb39a46ff754e2
Coulibaly Abdourahmane <abdou.coulibaly@outlook.fr>
2024-05-31 17:15:27 +0200

    by id routes added

--- src/routes/recipeRoutes.js
+++ src/routes/recipeRoutes.js
@@ -1,8 +1,9 @@
-import { getRecipeByIngredient, getRecipeByName,index} from '../controllers/recipeController.js'
+import { getRecipeById, getRecipeByIngredient, getRecipeByName,index} from '../controllers/recipeController.js'
  export default async function recipesRoutes (fastify,options){
     fastify.get('/',index)
-    fastify.get('/recipes/:name',getRecipeByName)
+    fastify.get('/recipes/name/:name',getRecipeByName)
     fastify.get('/recipes/ingredient/:ingredient',getRecipeByIngredient)
+    fastify.get('/recipes/id/:id',getRecipeById)
 
 }
 
